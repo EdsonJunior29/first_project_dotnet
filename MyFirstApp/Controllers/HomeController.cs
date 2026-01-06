26a338c3812fe391c305cb4608f95ebd95641f3c
EdsonJunior <edsonjos61@gmail.com>
2026-01-06 07:00:59 -0300

    Iniciando os estudos das controllers

---
+++ MyFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public string method1()
+        {
+            return "Hello from Index";
+        }
+    }
+}
