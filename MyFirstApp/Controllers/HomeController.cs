c4d9504eb2e53fd1c2ef6875bb7a073c3dfcd17e
EdsonJunior <edsonjos61@gmail.com>
2026-01-07 07:16:21 -0300

    Nesse commit foi adicionado o parametro de entrada de dados via URL.

--- MyFirstApp/Controllers/HomeController.cs
+++ MyFirstApp/Controllers/HomeController.cs
@@ -2,12 +2,19 @@
 
 namespace MyFirstApp.Controllers
 {
+    [Controller]
     public class HomeController : Controller
     {
         [Route("home")]
         public string method1()
         {
             return "Hello from Index";
+        }
+
+        [Route("contact-us/{mobile:int}")]
+        public string method2(int mobile)
+        {
+            return "Contact us at: " + mobile;
         }
     }
 }
